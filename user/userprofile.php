dbc778a14541baac6ecf3c9fafdfc51fc0166f8c
Sasa-rgb <shenkavi1120@gmail.com>
2024-06-28 21:52:39 +0530

    user profile bete

---
+++ user/userprofile.php
@@ -0,0 +1,91 @@
+<DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+    <link rel="stylesheet" href="../assets/css/ustyle.css">
+    </head>
+    <body>
+
+    <div class="profile-container">
+        <div class="profile-header">
+        <div class="profile-pic"></div>
+        <div class="user-details">
+                <h2>Sashen Kavinda</h2>
+                <div class="buttons">
+                    <button class="open-to">Open to</button>
+                    <button class="add-profile-section">Add profile section</button>
+                    <button class="more">More</button>
+                </div>
+            </div>
+        </div>
+        <div class="details">
+            <div class=about>
+            <h3>About me -_-</h3>
+                <p>Student at Cardiff Metropolitan University</p>
+                <p>Gampaha District, Western Province, Sri Lanka</p>
+                <p>Fitness Instructor, Information Technology Intern and Network Administrator roles</p>
+                <p></p>
+                <p>Aspiring scholar Sashen, currently a student, passionate about learning and driven to achieve excellence. Always eager to embrace new challenges and expand knowledge horizons.</p>
+            </div>
+        </div>
+        
+    </div>
+
+    <div class="exp-view">
+            <div class=exp-vv>
+            <h3>Experince-_-</h3>
+                <p>Labba</p>
+                <p>Labba</p>
+                <p>Labba</p>
+                <p>Labba</p>
+                <p>Labba</p>
+                <p>Labba</p>
+                <p>Labba</p>
+                <p>Labba</p>
+                <p>Labba</p>
+                <p>Labba</p>
+            </div>
+     </div>
+
+     <div class="job">
+        <h3>Job recommendations for you!</h3>
+        <div class="jobs-for-you">
+            <div class="jcard">Card</div>
+            <div class="jcard">Card</div>
+            <div class="jcard">Card</div>
+            <div class="jcard">Card</div>
+            <div class="jcard">Card</div>
+        </div>
+    </div>
+
+    <div class="organization">
+        <h3>Organizattion class</h3>
+        <div class="org">
+            <div class="ecard">Card</div>
+            <div class="ecard">Card</div>
+            <div class="ecard">Card</div>
+            <div class="ecard">Card</div>
+            <div class="ecard">Card</div>
+            <div class="ecard">Card</div>
+            <div class="ecard">Card</div>
+            <div class="ecard">Card</div>
+        </div>
+    </div>
+
+    <div class="courses">
+        <h3>Courses you learning</h3>
+        <div class="cou-for-you">
+            <div class="card">Card</div>
+            <div class="card">Card</div>
+            <div class="card">Card</div>
+            <div class="card">Card</div>
+            <div class="card">Card</div>
+        </div>
+    </div>
+
+
+    
+    </body>
+</html>
