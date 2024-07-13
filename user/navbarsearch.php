d84f6fcb7e067f09fbca8714ad2f7f874f6d617b
Pramushka <ninupramushka@gmail.com>
2024-07-14 02:43:22 +0530

    fixed jobsearch design

--- user/navbarsearch.php
+++ user/navbarsearch.php
@@ -25,7 +25,8 @@ while ($row = $positionResult->fetch_assoc()) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,700" rel="stylesheet">
-    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+
     <link rel="stylesheet" href="../assets/css/text.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
