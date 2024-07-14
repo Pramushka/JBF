0c4c1192c062105e55efc32e67273ba20bfae83c
sasa-rgb <shenkavi1120@gmail.com>
2024-07-14 12:01:26 +0530

    admin

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -89,6 +89,7 @@
             <li><a href="../admin/admin_jobpost_dashboard.php"><i class="fas fa-ticket-alt"></i> Open Tickets</a></li>
             <li><a href="../admin/open_tickets.php"><i class="fas fa-check-circle"></i> Closed Tickets</a></li>
             <li><a href="../admin/post_courses.php"><i class="fas fa-plus-circle"></i> Add Course</a></li>
+             <li><a href="../admin/NewsalertSubs.php"><i class="fas fa-exclamation-circle"></i> News Alert</a></li>
             <li><a href="../user/logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
         </ul>
     </nav>
